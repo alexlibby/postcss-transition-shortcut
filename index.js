2903ee45a2169dba55c2dc465988d3bd98743407
alexlibby <alex.libby@not42.net>
2015-12-31 23:05:26 +0000

    1.0

---
+++ index.js
@@ -0,0 +1,36 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-transition-shortcut', function (options) {
+	options = options || {};
+
+	var defaults = {
+		property: 'all',
+		duration: '1s',
+		timing: 'ease-in-out',
+		delay: '1s'
+	};
+
+	var splice = Array.prototype.splice;
+
+	return function (css) {
+		css.walkRules(function (rule) {
+			var transitionRule;
+			var transitionValues = [];
+			var index = -1, node;
+			var attributes = /^(property|duration|timing|delay)$/;
+
+			while (node = rule.nodes[++index]) {
+				if (attributes.test(node.prop)) {
+					transitionRule = transitionRule || node.cloneBefore({ prop: 'transition' });
+
+					var transValues = postcss.list.space(node.value);
+
+					transitionValues.push(transValues.join(','));
+					node.remove();
+					--index;
+				}
+			}
+			transitionRule.value = transitionValues.join(' ');			
+		});
+	};
+});
